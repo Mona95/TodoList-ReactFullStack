91ffce86009bbf3085ccc60066124d817b4e88b2
mzarkandi <mzarkandi@hotech.systems>
2020-03-27 22:40:30 +0300

    index.js added for needed folders

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React from "react";
 
-function App() {
-  return (
-    <div>
-      <header>TodoList Application</header>
-    </div>
-  );
-}
-
-export default App;
+export const App = () => (
+  <div>
+    <header>TodoList Application</header>
+  </div>
+);
