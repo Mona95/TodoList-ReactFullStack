366336dba31bc7869d1a36d69db2659ccd0c7997
Mona95 <shamona9594@gmail.com>
2020-03-29 14:10:44 +0300

    adding logic to get next 7 days of tasks + archived tasks

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -8,7 +8,8 @@ import { collectedTasksExist } from "../helpers";
  * @param {*} selectedProject
  */
 export const useTasks = selectedProject => {
-  const [tasks, settasks] = useState([]);
+  const [tasks, setTasks] = useState([]);
+  const [archivedTasks, setArchivedTasks] = useState([]);
   useEffect(() => {
     let unsubscribe = firebase
       .firebase()
@@ -27,5 +28,24 @@ export const useTasks = selectedProject => {
         : selectedProject === "INBOX" || selectedProject === 0
         ? (unsubscribe = unsubscribe.where("date", "==", ""))
         : unsubscribe;
+
+    unsubscribe = unsubscribe.onSnapshot(snapshot => {
+      const newTasks = snapshot.docs.map(task => ({
+        id: tasks.id,
+        ...task.data()
+      }));
+
+      setTasks(
+        selectedProject === "NEXT_7"
+          ? newTasks.filter(
+              task =>
+                moment(task.date, "DD-MM-YYYY").diff(moment(), "days") <= 7 &&
+                task.archived !== true
+            )
+          : newTasks.filter(task => task.archived !== true)
+      );
+
+      setArchivedTasks(newTasks.filter(task => task.archived !== false));
+    });
   }, [selectedProject]);
 };
